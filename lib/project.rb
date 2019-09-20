95b7e0a162c44fb9ee811e8d116f33730326da70
Damini Varu <damvaru4@gmail.com>
2019-09-20 19:06:00 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,10 @@
 class Project 
   
-  attr_reader :title 
+  attr_reader :title :backers
   
-  def initialize(title)
+  def initialize(title, backers)
     @title = title
+    @backers = []
   end 
   
 end 
